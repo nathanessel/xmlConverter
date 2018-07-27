9fe226df57d70a56ab0319de8868fe5e36b0c605
Nathan Essel <nathanessel@Nathans-Mac-mini.local>
2018-07-26 23:49:42 -0400

    Created .exe file for A-270

--- converter/src/converter/CsvConverter.java
+++ converter/src/converter/CsvConverter.java
@@ -4,8 +4,8 @@ public class CsvConverter {
 
     public static void main(String[] args) throws Exception {
 
-        String startFile = "/Users/nathanessel/DeV/xml/A-270_Sample_TXT_File_04_Nathan_2018_07-25-18.txt";
-        String outFile = "/Users/nathanessel/DeV/xml/A-270_Sample_TXT_File_04_Nathan_2018_07-25-18.xml";
+        String startFile = "C:\\A-270_Sample_TXT_File_04_Nathan_2018_07-25-18.txt";
+        String outFile = "C:\\A-270_Sample_TXT_File_04_Nathan_2018_07-25-18.xml";
 
         //XMLPartsCreator creator = new XMLPartsCreator();
         
