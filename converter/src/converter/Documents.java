f774651c4681eb8786a367e9b5fce0bb1ffa8555
Nathan Essel <nathanessel@Nathans-Mac-mini.local>
2018-07-30 00:46:51 -0400

    Fixed bug with creating and moving files and folders on Windows. Added log and error log logic to all conversions. Changed datatype for Activities and Documents for the Process Import to allow for multiple tags for Activity and Document for each Operation

---
+++ converter/src/converter/Documents.java
@@ -0,0 +1,10 @@
+package converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Documents {
+	String name = "";
+	List<ProcessRevisionDataElement> documentsList = new ArrayList<ProcessRevisionDataElement>();
+
+}
